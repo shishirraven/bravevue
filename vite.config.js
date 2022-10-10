2d983bffc04c521369cb33d748a38082bc48d6a5
Shishir Raven <shishir.raven@gmail.com>
2022-10-10 16:19:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: "/bravevue/",
+  base: "",
   plugins: [vue()],
   resolve: {
     alias: {
