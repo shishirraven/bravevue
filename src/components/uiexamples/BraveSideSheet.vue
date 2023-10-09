e0e5fb3b7981350ecdbfc3c8d1a5275c6ffb88a6
Shishir <shishir.raven@gmail.com>
2023-10-09 22:04:41 +0530

    s

--- src/components/uiexamples/BraveSideSheet.vue
+++ src/components/uiexamples/BraveSideSheet.vue
@@ -1,144 +1,225 @@
 <template>
-    <!-- Buttons to change the Sheets State to closed,half,full -->
-    <div class="flex lg:flex-row flex-col gap-2 mt-3">
-<!-- <button @click="state = 'closed'" type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center lg:flex-1 ">Close Bottom Sheet</button> -->
-<button @click="state = 'open'" type="button" class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center lg:flex-1 ">Open Side Sheet</button>
-       
-    </div>
-
-
-    <BraveSideSheet
-     ref="workspaces"
-      class="bg-slate-200  dark:bg-slate-900  "
-      v-model:state="stateWorkspace"
-      sheet-width="375px"
-      v-slot="{close}"
+  <!-- Buttons to change the Sheets State to closed,half,full -->
+  <div class="mt-3 flex flex-col gap-2 lg:flex-row">
+    <!-- <button @click="state = 'closed'" type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center lg:flex-1 ">Close Bottom Sheet</button> -->
+    <button
+      @click="state = 'open'"
+      type="button"
+      class="rounded-lg bg-gradient-to-r from-teal-200 to-lime-200 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:outline-none focus:ring-4 focus:ring-lime-200 dark:focus:ring-teal-700 lg:flex-1"
     >
+      Open Side Sheet
+    </button>
+  </div>
+
+  <BraveSideSheet
+    ref="workspaces"
+    class="bg-slate-200 dark:bg-slate-900"
+    v-model:state="stateWorkspace"
+    sheet-width="375px"
+    v-slot="{ close }"
+  >
     <div class="h-screen w-96 bg-slate-900">
-  <!-- Back Button -->
-  <div 
-  @click="close(),state = 'open'"
-  class="font-semibold flex items-center gap-2 bg-slate-800 p-5 text-slate-200 cursor-pointer">
-    <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
-      </svg>
+      <!-- Back Button -->
+      <div
+        @click="close(), (state = 'open')"
+        class="flex cursor-pointer items-center gap-2 bg-slate-800 p-5 font-semibold text-slate-200"
+      >
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-chevron-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+            />
+          </svg>
+        </div>
+        <div>Settings</div>
+      </div>
+      <!-- /Back Button -->
+
+      <!-- Workspace Settings -->
+      <div class="p-8">Workspace</div>
     </div>
-    <div>Settings</div>
-  </div>
-  <!-- /Back Button -->
+  </BraveSideSheet>
 
-  <!-- Workspace Settings -->
-   <div class="p-8">Workspace</div>
-
-
-  </div>
-
-    </BraveSideSheet>
-
-
-
-
-    <BraveSideSheet
-      class="bg-slate-200  dark:bg-slate-900  "
-      v-model:state="state"
-      sheet-width="375px"
-      v-slot="{close}"
-    >
+  <BraveSideSheet
+    class="bg-slate-200 dark:bg-slate-900"
+    v-model:state="state"
+    sheet-width="375px"
+    v-slot="{ close }"
+  >
     <div class="h-screen w-96 bg-slate-900">
-  <!-- Back Button -->
-  <div @click="close()" class="font-semibold flex items-center gap-2 bg-slate-800 p-5 text-slate-200 cursor-pointer">
-    <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
-      </svg>
+      <!-- Back Button -->
+      <div
+        @click="close()"
+        class="flex cursor-pointer items-center gap-2 bg-slate-800 p-5 font-semibold text-slate-200"
+      >
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-chevron-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+            />
+          </svg>
+        </div>
+        <div>Settings</div>
+      </div>
+      <!-- /Back Button -->
+
+      <!-- Profile Image and Icon -->
+      <div class="flex flex-col items-center justify-center bg-slate-800 p-5">
+        <img
+          class="h-24 w-24"
+          src="@/assets/images/shishir_profie.png"
+          alt=""
+        />
+        <div class="mt-5 text-2xl font-semibold text-white">Shishir Raven</div>
+        <div class="mt-1 text-sm text-slate-400">shishir.raven@gmail.com</div>
+      </div>
+      <!-- Profile Image and Icon -->
+
+      <!-- Navigation Example  -->
+
+      <div class="mt-5 bg-slate-900 py-10">
+        <!-- workspaes -->
+        <div
+          @click="(stateWorkspace = 'open'), close()"
+          class="flex cursor-pointer items-center gap-3 px-5 py-4 text-slate-100 hover:bg-gray-800"
+        >
+          <div class="text-slate-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              class="bi bi-building h-5 w-5"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1ZM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Z"
+              />
+              <path
+                d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V1Zm11 0H3v14h3v-2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V15h3V1Z"
+              />
+            </svg>
+          </div>
+          <div class="flex-grow font-semibold">Workspaces</div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+        </div>
+        <!-- /Workspaces -->
+
+        <!-- profile -->
+        <div
+          class="flex cursor-pointer items-center gap-3 px-5 py-4 text-slate-100 hover:bg-gray-800"
+        >
+          <div class="text-slate-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              class="bi bi-person-circle h-5 w-5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+              <path
+                fill-rule="evenodd"
+                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+              />
+            </svg>
+          </div>
+          <div class="flex-grow font-semibold">Profile</div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+        </div>
+        <!-- /profile -->
+        <!-- profile -->
+        <div
+          class="flex cursor-pointer items-center gap-3 px-5 py-4 text-slate-100 hover:bg-gray-800"
+        >
+          <div class="text-slate-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              class="bi bi-gear-fill h-5 w-5"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"
+              />
+            </svg>
+          </div>
+          <div class="flex-grow font-semibold">Preferences</div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </div>
+        </div>
+        <!-- /profile -->
+      </div>
     </div>
-    <div>Settings</div>
-  </div>
-  <!-- /Back Button -->
+  </BraveSideSheet>
+</template>
+<script>
+import { BraveSideSheet, SwipeUp, SwipeDown } from "bravevue";
 
-  <!-- Profile Image and Icon -->
-  <div class="flex flex-col items-center justify-center bg-slate-800 p-5">
-    <img class="h-24 w-24" src="@/assets/images/shishir_profie.png" alt="" />
-    <div class="mt-5 text-2xl font-semibold text-white">Shishir Raven</div>
-    <div class="mt-1 text-sm text-slate-400">shishir.raven@gmail.com</div>
-  </div>
-  <!-- Profile Image and Icon -->
-
-  <!-- Navigation Example  -->
-
-  <div class="mt-5 bg-slate-900 py-10">
-    <!-- workspaes -->
-    <div
-    @click="stateWorkspace = 'open',close()"
-    class="flex items-center gap-3 px-5 py-4 text-slate-100 hover:bg-gray-800 cursor-pointer ">
-      <div class="text-slate-400">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-building h-5 w-5" viewBox="0 0 16 16">
-          <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1ZM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Z" />
-          <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V1Zm11 0H3v14h3v-2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V15h3V1Z" />
-        </svg>
-      </div>
-      <div class="flex-grow font-semibold">Workspaces</div>
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
-        </svg>
-      </div>
-    </div>
-    <!-- /Workspaces -->
-
-    <!-- profile -->
-    <div class="flex items-center gap-3 px-5 py-4 text-slate-100 hover:bg-gray-800 cursor-pointer ">
-      <div class="text-slate-400">
-        <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi h-5 w-5 bi-person-circle" viewBox="0 0 16 16">
-          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-          <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-        </svg>
-      </div>
-      <div class="flex-grow font-semibold">Profile</div>
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
-        </svg>
-      </div>
-    </div>
-    <!-- /profile -->
-      <!-- profile -->
-    <div class="flex items-center gap-3 px-5 py-4 text-slate-100 hover:bg-gray-800 cursor-pointer ">
-      <div class="text-slate-400">
-       <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-gear-fill h-5 w-5" viewBox="0 0 16 16">
-  <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
-</svg>
-      </div>
-      <div class="flex-grow font-semibold">Preferences</div>
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
-        </svg>
-      </div>
-    </div>
-    <!-- /profile -->
-  </div>
-</div>
-
-
-      
-    </BraveSideSheet>
-  </template>
-  <script>
-  import { BraveSideSheet,SwipeUp, SwipeDown } from 'bravevue';
-
-  export default {
-
-    directives: { SwipeUp, SwipeDown },
-    components: {
-      BraveSideSheet,
-    },
-    data() {
-      return {
-        state: "closed",
-        stateWorkspace: "closed",
-      };
-    },
-  };
-  </script>
+export default {
+  directives: { SwipeUp, SwipeDown },
+  components: {
+    BraveSideSheet,
+  },
+  data() {
+    return {
+      state: "closed",
+      stateWorkspace: "closed",
+    };
+  },
+};
+</script>
